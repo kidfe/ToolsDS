3603ba6682fe733598ea2f7c2e7e27969dadb660
kidfe <126822364+kidfe@users.noreply.github.com>
2023-03-04 04:58:54 +0500

    Add files via upload

---
+++ Tools DS Assignment.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# <b>Nail Farkhatdinov</b>
+# <p>Data Scientist in Cultural Field
+
+# <p><i>I am interested in Data Science because it provides a solution-centred perspective on reality. Looking at huge amounts of data, a professional should reveal patterns to address a specific problem and provide a certain data-driven vision on how to deal with it. 
+
+# <h3>The following code will say hello to those who are not familiar with data science yet</h3>
+
+# In[2]:
+
+
+print("Welcome and hello to all who are new to Data Science")
+
+
+# <hr>
+# <a href=researcharts.ru>Department of Research Arts</a>
+# <ul>To start programming you need to install one of the following environments
+#     <li>Jupyter
+#     <li>R Studio
+# </ul>
+# <img src=https://f8n-production-collection-assets.imgix.net/0xB64112CD5a833F09a292568a7263ead1c854F6f2/1/nft.png?q=80&auto=format%2Ccompress&cs=srgb&w=3000&h=3000&fit=max width=300 align=center>
+# <blockquote>
+# Баю-баю, Машенька,<br>Тихое сердечко, <br>Проживешь ты страшненько. <br>И сгоришь, как свечка.
+# </blockquote>
+
+# In[ ]:
+
+
+
+
